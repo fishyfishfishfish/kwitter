3568b45ce4a2cbdfa0ab267be74911e762c5f876
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2022-11-11 20:43:13 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,3 +1,28 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyA84mKAacc2foCcwCNmz3hTuItP2VOTFss",
+      authDomain: "purplrhbdhgbdsuyzgyesno.firebaseapp.com",
+      databaseURL: "https://purplrhbdhgbdsuyzgyesno-default-rtdb.firebaseio.com",
+      projectId: "purplrhbdhgbdsuyzgyesno",
+      storageBucket: "purplrhbdhgbdsuyzgyesno.appspot.com",
+      messagingSenderId: "159058871931",
+      appId: "1:159058871931:web:af2bee33e7f696764d84de",
+      measurementId: "G-JJJPLZGM49"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
+
+    user_name=localStorage.getItem("user_name");
+    document.getElementById("user_name").innerHTML="welcome"+user_name+"!";
+    
+    function addroom(){
+      room_name=document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose:"adding room name"
+      });
+      localStorage.setItem("room_name",room_name);
+      window.location="kwitter_page.html";
+    }
 
 //ADD YOUR FIREBASE LINKS HERE
 
